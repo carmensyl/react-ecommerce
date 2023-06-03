6ca4ae155980747adc83f201a4569464ea28c8be
carmensyl <134837765+carmensyl@users.noreply.github.com>
2023-06-03 17:34:16 +0800

    big display wording

--- pages/index.js
+++ pages/index.js
@@ -33,13 +33,13 @@ const Home = ({ inventoryData = [], categories: categoryData = [] }) => {
         flex lg:flex-row flex-col"
         >
           <div className="pt-4 pl-2 sm:pt-12 sm:pl-12 flex flex-col">
-            <Tag year="2021" category="TV" />
+            <Tag year="2023" category="BEST BUY" />
             <Center
               price="200"
               title={inventory[2].name}
               link={`/product/${slugify(inventory[2].name)}`}
             />
-            <Footer designer="Jason Bourne" />
+            <Footer designer="Sony" />
           </div>
           <div className="flex flex-1 justify-center items-center relative">
             <Showcase imageSrc={inventory[2].image} />
